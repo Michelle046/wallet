4a2653720b716b9e12246068566e87f1085a5d62
Michelle <ehikhamhenmichelle2@gmail.com>
2025-09-18 22:13:56 +0100

    fix: updated config import paths

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-import { initDB } from "../config/db.js"
+import { initDB } from "./config/db.js"
 import rateLimiter from "./middleware/rateLimiter.js";
 
 import transactionsRoute from "./routes/transactionsRoute.js"
